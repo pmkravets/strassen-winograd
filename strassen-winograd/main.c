edadf192a06ba1e9c565194404275c4c6374ef54
Pavel <pmkravets@gmail.com>
2013-10-03 01:11:36 +0400

    Initial Commit

---
+++ strassen-winograd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  strassen-winograd
+//
+//  Created by Pavel Kravets on 03.10.13.
+//  Copyright (c) 2013 Pavel Kravets. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
